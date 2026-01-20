5517d27edc3c78c4fd5dd850bc0b98b53dae5cb0
Gianluca Rea <reagianluca97@gmail.com>
2026-01-20 17:07:02 +0100

    [any message]

--- app/main.go
+++ app/main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
+	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("$ ")
+	command, _ := reader.ReadString('\n')
+	command = strings.TrimSpace(command)
+	fmt.Printf("%s: command not found \n", command)
 }
