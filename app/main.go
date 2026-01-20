8ba2771cc9126530a89e4c8325ba6a8d7ba05a50
Gianluca Rea <reagianluca97@gmail.com>
2026-01-20 17:42:59 +0100

    [any message]

--- app/main.go
+++ app/main.go
@@ -7,24 +7,57 @@ import (
 	"strings"
 )
 
-var commandList = map[string]int{"exit": 1}
+type Command struct {
+	name    string
+	execute func() error
+}
+
+var commandList = map[string]Command{
+	"exit": {name: "exit", execute: func() error { return nil }},
+	"echo": {name: "echo", execute: func() error { return nil }},
+}
+
+func printPrompt() {
+	fmt.Print("$ ")
+}
 
 func main() {
-	fmt.Print("$ ")
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	printPrompt()
+	handleInput()
+	main()
+}
+
+func handleInput() {
+	input, err := bufio.NewReader(os.Stdin).ReadString('\n')
 
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Error reading input:", err)
 		os.Exit(1)
 	}
 
-	command = strings.TrimSpace(command)
-	commandType, exists := commandList[command]
-	if !exists {
-		fmt.Println(command + ": command not found")
+	input = strings.TrimSpace(input)
+	parts := strings.Fields(input)
+
+	if len(parts) <= 0 {
+		return
 	}
-	if commandType == 1 {
-		os.Exit(0)
+
+	commandAsString := parts[0]
+	args := parts[1:]
+
+	if command, exists := commandList[commandAsString]; exists {
+		if command.name == "exit" {
+			os.Exit(0)
+		}
+
+		if command.name == "echo" {
+			fmt.Fprintln(os.Stdout, strings.Join(args, " "))
+		}
+
+		if err := command.execute(); err != nil {
+			fmt.Fprintln(os.Stderr, "Error executing command:", err)
+		}
+	} else {
+		fmt.Fprintf(os.Stdout, "%s: command not found\n", commandAsString)
 	}
-	main()
 }
